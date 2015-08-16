55af1869f4b58fb6c08af236661b8a7e57717e20
jarrodtarico <jarrod.parks93@gmail.com>
2015-08-15 21:21:34 -0400

    Heroku problem2

--- coffeed/wsgi.py
+++ coffeed/wsgi.py
@@ -14,4 +14,4 @@ from dj_static import cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeed.settings")
 
-application = cling(get_wsgi_application())
+application = Cling(get_wsgi_application())
